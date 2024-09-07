5e0a49f648bad9eaf1e08f5f49e23dd364abc73e
Alan Jose Santo <realityaudiostudio@gmail.com>
2024-09-07 21:34:33 +0530

    Events page like netflix

--- src/pages/Events/Events.jsx
+++ src/pages/Events/Events.jsx
@@ -1,10 +1,17 @@
+import EventInd from "../../components/Events/EventInd"
 import EventsNav from "../../components/Events/EventsNav"
+import Footer from "../../components/Footer/Footer"
+import Navbar from "../../components/Navbar/Navbar"
 import "./Events.css"
 const Events = () => {
   return (
     <>
-    <EventsNav/>
+    <Navbar/>
+    <h3 className="evhd">Events</h3>
     
+    <EventsNav/>
+    <EventInd/>
+    <Footer/>    
     </>
   )
 }
