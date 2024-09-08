d841d4eea6189a880633998608869a56c8454b21
Alan Jose Santo <realityaudiostudio@gmail.com>
2024-09-08 13:02:21 +0530

    Sponsor edit

--- src/pages/Sponsor/Sponsor.jsx
+++ src/pages/Sponsor/Sponsor.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './sponsor.css';
+import "./sponsor.css";
 import Navbar from '../../components/Navbar/Navbar'
 import Footer from '../../components/Footer/Footer'
 import ThSpon from '../../img/spons.png';
