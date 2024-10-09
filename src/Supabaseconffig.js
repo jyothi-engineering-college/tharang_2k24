833067f25d27f0fc150e0d6d5776533e3baeb72f
ARUN JOHN  M S <157452839+Runa8147@users.noreply.github.com>
2024-10-09 17:38:15 +0530

    Update Supabaseconffig.js

--- src/Supabaseconffig.js
+++ src/Supabaseconffig.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://byuubvkdiddwygztuujz.supabase.co";
+const supabaseUrl = "supabase_url";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ5dXVidmtkaWRkd3lnenR1dWp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NDMxMjUsImV4cCI6MjA0MTUxOTEyNX0.DC_eRfQkvKDMMkxZKWi8grByoVj1QV889lY5RiVhRYQ";
+  "supabaseKey";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
